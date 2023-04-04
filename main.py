008ce71db5773a1e85e045534dbffac0044eba2e
Mounika Reddy <jmounika978@gmail.com>
2023-04-04 15:44:03 -0400

    The main method is added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ('Hello Team !')
+from service.tickTacToeGame import TicTacToeGame
+from service.ticTacToeBoard import TicTacToeBoard
+
+
+def main():
+    """Create the game's board and run its main loop."""
+    game = TicTacToeGame()
+    board = TicTacToeBoard(game)
+    board.mainloop()
+
+
+if __name__ == "__main__":
+    main()
